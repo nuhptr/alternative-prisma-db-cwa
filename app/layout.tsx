6cdcfe9fc4dfea4a3f4e98206b9a8d7753bac675
nuhptr <nugrahaadi733@gmail.com>
2024-07-08 09:14:10 +0700

    refactor: :recycle: update code and package manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,14 +6,15 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-    title: "Alternative Prisma DB for Replace Planetscale",
-    description: "This is a simple example of a Next.js app with Prisma and a custom database provider.",
+   title: "Alternative Prisma DB for Replace Planetscale",
+   description:
+      "This is a simple example of a Next.js app with Prisma and a custom database provider.",
 }
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-    return (
-        <html lang="en">
-            <body className={inter.className}>{children}</body>
-        </html>
-    )
+   return (
+      <html lang="en">
+         <body className={inter.className}>{children}</body>
+      </html>
+   )
 }
